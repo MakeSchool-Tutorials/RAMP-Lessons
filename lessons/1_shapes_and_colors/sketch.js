9fcaad0b49c2afbb6882a8ffb59a86c2608d03e5
Jordan Arnesen <jordan101112@gmail.com>
2018-06-03 16:10:11 -0400

    Lesson setup

---
+++ lessons/1_shapes_and_colors/sketch.js
@@ -0,0 +1,15 @@
+function setup () {
+  createCanvas(400, 400);
+}
+
+function draw () {
+  helperBackground(); // do not change or move this line
+
+  // >>>>>> Your code BELOW here >>>>>>
+  // #2 Draw Creature 1
+
+  // #2 Draw Creature 2
+
+  // Bonus Challenge
+  // <<<<<< Your code ABOVE here <<<<<<
+}
