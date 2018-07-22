42c2124b25bd2387b536816cc8e352c79bfca289
Jordan Arnesen <jordan101112@gmail.com>
2018-07-22 14:10:14 -0700

    Remove temp code

--- lessons/4_conditionals/sketch_sol.js
+++ lessons/4_conditionals/sketch_sol.js
@@ -3,13 +3,10 @@ var color1;
 var color2;
 var color3;
 var oneThird;
-var go;
 
 function setup () {
   createCanvas(400, 200);
   helperVariables(); // assigns global variable values other than beast
-
-  go = true;
 
   beast = {
     x: 50,
@@ -25,19 +22,9 @@ function draw () {
   helperBackground();
 
   // #2 Update beast position variables
-
-  // #3 update the beast color to match the background
-
-  // #4 and #5 Update message if chasing or fleeing the mouse
-
-  // Bonus 1 and 2
-
-    // #4 Update message if chasing or fleeing the mouse
-  var movingTowardsMouse = (beast.speed > 0 && beast.x < mouseX) || (beast.speed < 0 && beast.x > mouseX);
-  if (movingTowardsMouse) {
-    beast.message = "Chasing mouse";
-  } else if (!movingTowardsMouse) {
-    beast.message = "Fleeing mouse";
+  beast.x = beast.speed + beast.x;
+  if (beast.x > width || beast.x < 0) {
+    beast.speed = beast.speed * -1;
   }
 
   // #3 update the beast color to match the background
@@ -49,25 +36,16 @@ function draw () {
     beast.color = color3;
   }
 
-  // #2 Finish the bouncing beast
-  beast.x = beast.speed + beast.x;
-  if (beast.x > width || beast.x < 0) {
-    beast.speed = beast.speed * -1;
+  // #4 and #5 Update message if chasing or fleeing the mouse
+  var movingTowardsMouse = (beast.speed > 0 && beast.x < mouseX) || (beast.speed < 0 && beast.x > mouseX);
+  if (movingTowardsMouse) {
+    beast.message = "Chasing mouse";
+  } else if (!movingTowardsMouse) {
+    beast.message = "Fleeing mouse";
   }
 
-  fill(255);
-  text(beast.message, 10, 20);
+  // Bonus 1 and 2
 
   helperDrawBeast();
   helperText();
-}
-
-function mousePressed() {
-  if (go) {
-    go = false;
-    noLoop();
-  } else {
-    go = true;
-    loop();
-  }
 }
